7962cf0351d9ee452241ddf19e0a31abe5e7cb51
LapisMYT <nikitagavrilin005@gmail.com>
2024-01-27 21:52:12 +0300

    .

--- bot.py
+++ bot.py
@@ -1,91 +1,110 @@
 import os
-import telebot
 import requests
+from PIL import Image
+from io import BytesIO
+import telebot
 
 with open("tg_token.txt") as f:
     token = f.read().strip()
 
+
 bot = telebot.TeleBot(token)
 session = requests.Session()
 
+apis = [
+    "api.rule34.xxx",
+    "gelbooru.com",
+    "safebooru.org"
+]
+
 @bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, "Привет! Введите имя датасета:")
-    bot.register_next_step_handler(message, get_dataset_name)
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Какой датасет вы хотите собрать?")
+    bot.register_next_step_handler(message, process_dataset_name_step)
 
-def get_dataset_name(message):
-    dataset_name = message.text
-    bot.send_message(message.chat.id, "Введите триггер-тег:")
-    bot.register_next_step_handler(message, get_trigger_tag, dataset_name)
+def process_dataset_name_step(message):
+    try:
+        chat_id = message.chat.id
+        dataset_name = message.text
+        dataset_folder = f"./dataset/{dataset_name}"
+        if not os.path.exists(dataset_folder):
+            os.makedirs(dataset_folder)
+        
+        msg = bot.send_message(chat_id, 'Сколько изображений скачать?')
+        bot.register_next_step_handler(msg, process_image_limit_step, dataset_folder)
+    except Exception as e:
+        bot.reply_to(message, 'oooops')
 
-def get_trigger_tag(message, dataset_name):
-    trigger_tag = message.text
-    bot.send_message(message.chat.id, "Введите теги для поиска (через запятую):")
-    bot.register_next_step_handler(message, get_search_tags, dataset_name, trigger_tag)
+def process_image_limit_step(message, dataset_folder):
+    try:
+        chat_id = message.chat.id
+        image_limit = int(message.text)
+        
+        msg = bot.send_message(chat_id, 'Введите триггер::')
+        bot.register_next_step_handler(msg, process_tags_step, dataset_folder, image_limit)
+    except Exception as e:
+        bot.reply_to(message, 'Ошибка ввода. Попробуйте ввести число ещё раз.')
+        bot.register_next_step_handler(message, process_image_limit_step, dataset_folder)
 
-def get_search_tags(message, dataset_name, trigger_tag):
-    search_tags = message.text.strip().split(" ")
-    search_tags = [tag.strip() for tag in search_tags]
+def process_tags_step(message, dataset_folder, image_limit):
+    try:
+        chat_id = message.chat.id
+        tags = message.text.strip().replace(', ', '+').replace(' ', '_')
+        bot.send_message(message.chat.id, "Собираю изображения...")
+        images_downloaded = download_images(dataset_folder, tags, image_limit)
+        bot.send_message(chat_id, f'Загружено {images_downloaded} изображений.')
+    except Exception as e:
+        bot.reply_to(message, 'oooops')
+        print(repr(e))
 
-    dataset_path = os.path.join("dataset", dataset_name)
-    if not os.path.exists(dataset_path):
-        os.makedirs(dataset_path)
 
-    num_images_saved = download_images(message, dataset_path, search_tags, trigger_tag, 100)
-    
-    bot.send_message(message.chat.id, f"Датасет '{dataset_name}' собран. Найдено {num_images_saved} изображений.")
-
-def download_images(message, dataset_path, search_tags, trigger_tag, dataset_limit):
+def download_images(dataset_folder, tags, image_limit):
+    global apis
     num_images_saved = 0
     page_number = 1
-    bot.send_message(message.chat.id, f"Работаю...")
+    while num_images_saved < image_limit:
+        url = f"https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&json=1&limit=100&tags={tags}&pid={page_number}"
+        print(f"Page URL: {url}")
+        response = session.get(url)
+        posts = response.json()
 
-    while num_images_saved < dataset_limit:
-        try:
-            url = f"https://danbooru.donmai.us/posts.json?limit=100&page={page_number}&tags={'+'.join(search_tags)}"
-            print(f"Trying with url: {url}")
-            response = session.get(url)
-            response.raise_for_status()
-            data = response.json()
+        if not posts:
+            break
 
-            if not data:
+        #if int(posts["@attributes"]["count"]) < image_limit:
+        #    image_limit = int(posts["@attributes"]["count"])
+
+        #if not "post" in posts.keys():
+        #    break
+
+        for post in posts:
+            if num_images_saved >= image_limit:
                 break
-
-            for post in data:
-                if " ".join(search_tags) not in post["tag_string"]:
-                    continue
-
-                image_url = post.get("file_url")
+            
+            image_url = post['file_url']
+            if not image_url or not image_url.endswith(('.jpg', '.jpeg', '.png')):
+                continue
+            
+            try:
                 print(f"Image URL: {image_url}")
-                if not image_url:
-                    continue
+                response = session.get(image_url, stream=True)
+                response.raise_for_status()
 
-                if not image_url.endswith(".png"):
-                    continue
-
-                image_response = session.get(image_url, stream=True)
-                if image_response.status_code == 200:
-                    image_path = os.path.join(dataset_path, f"{num_images_saved}.png")
-                    with open(image_path, 'wb') as f:
-                        for chunk in image_response.iter_content(16384):
-                            f.write(chunk)
-
-                    tags = post["tag_string"].split()
-                    tags_path = os.path.join(dataset_path, f"{num_images_saved}.txt")
-                    with open(tags_path, "w") as f:
-                        f.write(f"{trigger_tag}, {', '.join(tags)}".replace("_", " "))
-
+                with Image.open(BytesIO(response.content)) as img:
+                    if img.format in ['JPEG', 'JPG']:
+                        img = img.convert('RGB')
+                    
                     num_images_saved += 1
-                    if num_images_saved >= dataset_limit:
-                        break
+                    filename = os.path.join(dataset_folder, f'{num_images_saved}.png')
+                    img.save(filename, 'PNG')
+                with open(os.path.join(dataset_folder, f"{num_images_saved}.txt"), "w") as f:
+                    f.write(f"{tags}, {post['tags']}")
 
-            page_number += 1
-            if num_images_saved >= dataset_limit:
-                break
+            except Exception as e:
+                print(f"Could not download image. Error: {e}")
 
-        except requests.exceptions.RequestException as e:
-            print(repr(e))
-
+        page_number += 1
+    
     return num_images_saved
 
-bot.infinity_polling()
+bot.infinity_polling(timeout = 20, long_polling_timeout = 10)
